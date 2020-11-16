ba0357bffc283ae712652763268b05bd066d58da
BISOON <20063083+BISOON@users.noreply.github.com>
2020-11-16 17:01:53 +0300

    init

---
+++ test/main.test.ts
@@ -0,0 +1,9 @@
+import {expect} from 'chai';
+
+//make sure everything good
+
+describe('main', () => {
+    it('should be true', async () => {
+        expect(true).to.be.true;
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+function rules()
+{
+    return [
+        {
+            test: /\.ts?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        },
+    ];
+}
+
+module.exports = {
+    entry: './src/main.ts',
+    module: {
+        rules: rules(),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
